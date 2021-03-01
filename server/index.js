72d63f0b8bbd1e28cd58f8edd3f9f5bebac59445
Атюганов Максим <atyuganov@o-code.ru>
2021-03-01 15:42:00 +0300

    Add connection with database, Add env files ingit-ignore

--- server/index.js
+++ server/index.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
 
+require("dotenv").config();
+const db = require("./core/database");
+
 app.listen(process.env.PORT, () => {
-  console.log("Server running");
+  try {
+    // Testing the connection
+    db.authenticate();
+    console.log("Server is running");
+  } catch (error) {
+    console.log(error);
+  }
 });
